88f8ec63b9ecd36046094bf4bc27cf76744632cc
anegin <anegamega@gmail.com>
2022-10-04 21:21:32 +0300

    Lab2: add placeholder Home page.

--- lab_2/src/App.js
+++ lab_2/src/App.js
@@ -2,12 +2,13 @@ import React from "react";
 import './App.css';
 import {Route, Routes} from "react-router-dom";
 import Layout from "./pages/Layout/Layout";
+import Home from "./pages/Home/Home";
 
 function App() {
     return (
         <Routes>
             <Route path="/" element={<Layout/>}>
-                {/*<Route index element={}/>*/}
+                <Route index element={<Home />}/>
             </Route>
         </Routes>
     );
