2755fbdd934ea1aef6e5f292ff2e4fd824c11b33
anegin <anegamega@gmail.com>
2022-10-05 15:51:02 +0300

    Lab2: refactor Layout component styles.

--- lab_2/src/pages/Layout/Layout.js
+++ lab_2/src/pages/Layout/Layout.js
@@ -2,12 +2,13 @@ import React from 'react';
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import {Outlet} from "react-router-dom";
+import './Layout.css';
 
 const Layout = () => (
     <>
-        <Header />
-        <Outlet />
-        <Footer />
+        <Header/>
+        <Outlet/>
+        <Footer/>
     </>
 );
 
