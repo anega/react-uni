d09e0bd8403d6a1c8d22e89614de6b7f8cfaa2bb
anegin <anegamega@gmail.com>
2022-10-05 15:36:15 +0300

    Lab2: add About Us sample route, component.

--- lab_2/src/App.js
+++ lab_2/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import {Route, Routes} from "react-router-dom";
 import Layout from "./pages/Layout/Layout";
 import Home from "./pages/Home/Home";
+import AboutUs from "./pages/AboutUs/AboutUs";
 
 function App() {
     return (
         <Routes>
             <Route path="/" element={<Layout/>}>
-                <Route index element={<Home />}/>
+                <Route index element={<Home/>}/>
+                <Route path="about-us" element={<AboutUs/>}/>
             </Route>
         </Routes>
     );
