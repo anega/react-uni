95a880cf66f89dcaa390152521a2e37ccbc7be6c
anegin <anegamega@gmail.com>
2022-10-05 19:22:35 +0300

    Lab2: add homepage HowWeWork section component.

---
+++ lab_2/src/components/Home/HowWeWork/HowWeWork.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './HowWeWork.css';
+import {IconNum01, IconNum02, IconNum03, IconNum04} from '../../../assets/images';
+
+const HowWeWork = () => (
+    <div className="section how-we-work">
+        <div className="section-inner">
+            <div className="how-we-work-info">
+                <h2>How we work</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+                <a href="#" className="label arrow-link">Get in touch with us ⟶</a>
+            </div>
+            <div className="work-strategy-list">
+                <div className="work-strategy-item">
+                    <img src={IconNum01} alt=""/>
+                        <h5>Strategy</h5>
+                        <p>Euismod faucibus turpis eu gravida mi. Pellentesque et velit aliquam.</p>
+                </div>
+                <div className="work-strategy-item">
+                    <img src={IconNum02} alt=""/>
+                        <h5>Wireframing</h5>
+                        <p>Euismod faucibus turpis eu gravida mi. Pellentesque et velit aliquam.</p>
+                </div>
+                <div className="work-strategy-item">
+                    <img src={IconNum03} alt=""/>
+                        <h5>Design</h5>
+                        <p>Euismod faucibus turpis eu gravida mi. Pellentesque et velit aliquam.</p>
+                </div>
+                <div className="work-strategy-item">
+                    <img src={IconNum04} alt=""/>
+                        <h5>Development</h5>
+                        <p>Euismod faucibus turpis eu gravida mi. Pellentesque et velit aliquam.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+);
+
+export default HowWeWork;
--- lab_2/src/pages/Home/Home.js
+++ lab_2/src/pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from "../../components/Home/Banner/Banner";
+import HowWeWork from "../../components/Home/HowWeWork/HowWeWork";
 
 const Home = () => {
     return (
         <>
             <Banner/>
+            <HowWeWork/>
         </>
     );
 };
