7dc772f690bc5d643518ad7881fef5217ac48135
anegin <anegamega@gmail.com>
2022-10-05 19:20:47 +0300

    Lab2: add images export.

---
+++ lab_2/src/assets/images/index.js
@@ -0,0 +1,55 @@
+import AboutUs from './about-us.jpg';
+import Blog01 from './blog-01.jpg';
+import Blog02 from './blog-02.jpg';
+import Blog03 from './blog-03.jpg';
+import HeroHome from './hero-home.png';
+import IconNum01 from './icon-01.png';
+import IconNum02 from './icon-02.png';
+import IconNum03 from './icon-03.png';
+import IconNum04 from './icon-04.png';
+import LogoImg from './logo.svg';
+import Company01 from './logo-01.jpg';
+import Company02 from './logo-02.jpg';
+import Company03 from './logo-03.jpg';
+import Company04 from './logo-04.jpg';
+import Company05 from './logo-05.jpg';
+import Mission from './our-mission.jpg';
+import Vision from './our-vision.jpg';
+import Project01 from './project-01.jpg';
+import Project02 from './project-02.jpg';
+import Project03 from './project-03.jpg';
+import Team01 from './team-01.jpg';
+import Team02 from './team-02.jpg';
+import Team03 from './team-03.jpg';
+import Team04 from './team-04.jpg';
+import WhoWeAre from './who-we-are.jpg';
+import WomenWriting from './woman-writing-on-paper.jpg';
+
+export {
+    AboutUs,
+    Blog01,
+    Blog02,
+    Blog03,
+    HeroHome,
+    IconNum01,
+    IconNum02,
+    IconNum03,
+    IconNum04,
+    LogoImg,
+    Company01,
+    Company02,
+    Company03,
+    Company04,
+    Company05,
+    Mission,
+    Vision,
+    Project01,
+    Project02,
+    Project03,
+    Team01,
+    Team02,
+    Team03,
+    Team04,
+    WhoWeAre,
+    WomenWriting
+}
