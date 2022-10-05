58dc860fd2676366fa90454528742a06b0935674
anegin <anegamega@gmail.com>
2022-10-05 20:49:37 +0300

    Lab2: add about us Process section component.

---
+++ lab_2/src/components/AboutUs/Process/Process.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './Process.css';
+
+const Process = () => (
+    <div className="section process">
+        <div className="section-inner">
+            <h2>The process we follow</h2>
+            <div className="process-list">
+                <div className="process-item">
+                    <div className="process-bullet"></div>
+                    <h6>Planning</h6>
+                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr.</p>
+                </div>
+                <div className="process-item">
+                    <div className="process-bullet"></div>
+                    <h6>Conception</h6>
+                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr.</p>
+                </div>
+                <div className="process-item">
+                    <div className="process-bullet"></div>
+                    <h6>Design</h6>
+                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr.</p>
+                </div>
+                <div className="process-item">
+                    <div className="process-bullet"></div>
+                    <h6>Development</h6>
+                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+);
+
+export default Process;
--- lab_2/src/pages/AboutUs/AboutUs.js
+++ lab_2/src/pages/AboutUs/AboutUs.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {default as AboutUsComponent} from '../../components/AboutUs/AboutUs/AboutUs';
 import WhoWeAre from "../../components/AboutUs/WhoWeAre/WhoWeAre";
+import Process from "../../components/AboutUs/Process/Process";
 
 const AboutUs = () => {
     return (
         <>
             <AboutUsComponent/>
             <WhoWeAre/>
+            <Process/>
         </>
     );
 };
