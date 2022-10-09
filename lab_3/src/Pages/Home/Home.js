a4ddc0f12327a03683401ee2082d2af7f8eb2a75
anegin <anegamega@gmail.com>
2022-10-09 21:12:07 +0300

    Lab3: refactor homepage(simple reformat).

--- lab_3/src/Pages/Home/Home.js
+++ lab_3/src/Pages/Home/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from "../../components/Header/Header";
-import Footer from "../../components/Footer/Footer";
 import PaletteList from "../../components/PaletteList/PaletteList";
+import Footer from "../../components/Footer/Footer";
 
 const Home = () => (
     <div className="home-page">
