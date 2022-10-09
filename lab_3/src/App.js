bd51a2ef4d586be1d247ab07f22f9f3a3d628356
anegin <anegamega@gmail.com>
2022-10-09 20:20:45 +0300

    Lab3: add Homepage layout placeholder.

--- lab_3/src/App.js
+++ lab_3/src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import {Route, Routes} from "react-router-dom";
+import Home from "./Pages/Home/Home";
 
 const App = () => (
-    <div>App</div>
+    <Routes>
+        <Route path="/" element={<Home/>}/>
+    </Routes>
 );
 
 export default App;
