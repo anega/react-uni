46c31e2ed19ce9784bb92a7411080be5fa76fb60
anegin <anegamega@gmail.com>
2022-10-10 14:33:43 +0300

    Lab3: add palette tile component and styles.

--- lab_3/src/components/Palette/Palette.js
+++ lab_3/src/components/Palette/Palette.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import PaletteColorsPreview from "../PaletteColorsPreview/PaletteColorsPreview";
+import './Palette.css';
+import {Link} from "react-router-dom";
 
 const Palette = ({palette}) => (
-    <div>
-        <PaletteColorsPreview colors={palette.colors}/>
-        <div className="palette-info">
-            <h2>{palette.paletteName}</h2>
-            <span className="palette-emoji">{palette.emoji}</span>
-        </div>
-    </div>
+        <Link to="/" className="palette-item">
+            <PaletteColorsPreview colors={palette.colors}/>
+            <div className="palette-info">
+                <h2>{palette.paletteName}</h2>
+                <span className="palette-emoji">{palette.emoji}</span>
+            </div>
+        </Link>
 );
 
 export default Palette;
