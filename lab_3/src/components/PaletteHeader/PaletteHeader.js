7b7be18f1d9cab27527295c38b695c0b0f8737f1
anegin <anegamega@gmail.com>
2022-10-10 21:49:01 +0300

    Lab3: style color format selector btn.

--- lab_3/src/components/PaletteHeader/PaletteHeader.js
+++ lab_3/src/components/PaletteHeader/PaletteHeader.js
@@ -7,7 +7,13 @@ const PaletteHeader = () => {
         <header className="palette-header">
             <Link to={".."} className="palette-back-btn">⟵ Back</Link>
             <div className="color-format">
-                <p>Copy format: HEX (#AA1923)</p>
+                <p className="active-color-format">Copy Format: HEX (#AA1923)</p>
+                {/*<ul className="color-format-list">*/}
+                {/*    <li>HEX (#AA1923)</li>*/}
+                {/*    <li>HEX (AA1923)</li>*/}
+                {/*    <li>RGB - (1,2,3)</li>*/}
+                {/*    <li>RGBA - (1,2,3,0.4)</li>*/}
+                {/*</ul>*/}
             </div>
             <div className="sound-btn">Sound On 🔊</div>
         </header>
