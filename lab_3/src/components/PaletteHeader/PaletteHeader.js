c94dd512df20f0cd8707a4395becd23871a487eb
anegin <anegamega@gmail.com>
2022-10-12 19:07:24 +0300

    Lab3: change dropdown text when selecting another format.

--- lab_3/src/components/PaletteHeader/PaletteHeader.js
+++ lab_3/src/components/PaletteHeader/PaletteHeader.js
@@ -4,21 +4,42 @@ import './PaletteHeader.css';
 
 const PaletteHeader = ({isMuted, onClick}) => {
     const [showDropdown, setShowDropdown] = useState(false);
+    const [copyFormatText, setCopyFormatText] = useState('HEX (#AA1923)');
 
     function handleShowDropdown() {
         setShowDropdown((prevState) => !prevState);
+    }
+
+    function handleFormatChange(event) {
+        setCopyFormatText((prevState) => event.target.textContent);
     }
 
     return (
         <header className="palette-header">
             <Link to={".."} className="palette-back-btn">⟵ Back</Link>
             <div className={`color-format ${showDropdown ? 'show' : ''}`} onClick={handleShowDropdown}>
-                <p className="active-color-format">Copy Format: HEX (#AA1923)</p>
+                <p className="active-color-format">Copy Format: {copyFormatText}</p>
                 <ul className="color-format-list">
-                    <li value="hex">HEX (#AA1923)</li>
-                    <li value="hexNoHash">HEX (AA1923)</li>
-                    <li value="rgb">RGB - (1,2,3)</li>
-                    <li value="rgba">RGBA - (1,2,3,0.4)</li>
+                    <li
+                        onClick={(event) => handleFormatChange(event)}
+                        value="hex">
+                        HEX (#AA1923)
+                    </li>
+                    <li
+                        onClick={(event) => handleFormatChange(event)}
+                        value="hexNo
+                        Hash">HEX (AA1923)
+                    </li>
+                    <li
+                        onClick={(event) => handleFormatChange(event)}
+                        value="rgb">
+                        RGB - (1,2,3)
+                    </li>
+                    <li
+                        onClick={(event) => handleFormatChange(event)}
+                        value="rgba">
+                        RGBA - (1,2,3,0.4)
+                    </li>
                 </ul>
             </div>
             <div className="sound-btn" onClick={onClick}>Sound {`${isMuted ? 'Off 🔇' : 'On 🔊'}`}</div>
