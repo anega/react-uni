bdb10b7948f72e742e8e69d0a613f36471a56012
anegin <anegamega@gmail.com>
2022-10-12 13:43:57 +0300

    Lab2: remove commented code.

--- lab_2/src/components/Home/Projects/Projects.js
+++ lab_2/src/components/Home/Projects/Projects.js
@@ -14,39 +14,6 @@ const Projects = () => (
                 {data.map((project) => (
                     <Project key={project.id} project={project}/>
                 ))}
-
-                {/*<div className="main-gallery-item">
-                    <img src={Project01} alt="Finsweet project"/>
-                    <div className="main-gallery-item-info-wrap">
-                        <div className="main-gallery-item-info">
-                            <h6>Workhub office Webflow Webflow Design</h6>
-                            <p className="text-semi-bold">Euismod faucibus turpis eu gravida mi. Pellentesque et
-                                velit
-                                aliquam</p>
-                            <a href="/#" className="text-semi-bold arrow-link">View project ⟶</a>
-                        </div>
-                    </div>
-                </div>
-                <div className="projects-gallery-secondary">
-                    <div className="projects-gallery-item">
-                        <img src={Project02} alt="Finsweet project"/>
-                        <div className="projects-gallery-item-info-wrap">
-                            <div className="projects-gallery-item-info">
-                                <h6>Unisaas Website Design</h6>
-                                <a href="/#" className="text-semi-bold arrow-link">View portfolio ⟶</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="projects-gallery-item">
-                        <img src={Project03} alt="Finsweet project"/>
-                        <div className="projects-gallery-item-info-wrap">
-                            <div className="projects-gallery-item-info">
-                                <h6>Another title</h6>
-                                <a href="/#" className="text-semi-bold arrow-link">View portfolio ⟶</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>*/}
             </div>
         </div>
     </div>
