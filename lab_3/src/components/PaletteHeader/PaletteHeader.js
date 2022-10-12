363b1aad03803bb8147bbe0857d90a553499cb39
anegin <anegamega@gmail.com>
2022-10-12 18:42:16 +0300

    Lab3: handle color format selector dropdown(open/close).

--- lab_3/src/components/PaletteHeader/PaletteHeader.js
+++ lab_3/src/components/PaletteHeader/PaletteHeader.js
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link} from "react-router-dom";
 import './PaletteHeader.css';
 
 const PaletteHeader = ({isMuted, onClick}) => {
+    const [showDropdown, setShowDropdown] = useState(false);
+
+    function handleShowDropdown() {
+        setShowDropdown((prevState) => !prevState);
+    }
+
     return (
         <header className="palette-header">
             <Link to={".."} className="palette-back-btn">⟵ Back</Link>
-            <div className="color-format">
+            <div className={`color-format ${showDropdown ? 'show' : ''}`} onClick={handleShowDropdown}>
                 <p className="active-color-format">Copy Format: HEX (#AA1923)</p>
                 <ul className="color-format-list">
-                    <li>HEX (#AA1923)</li>
-                    <li>HEX (AA1923)</li>
-                    <li>RGB - (1,2,3)</li>
-                    <li>RGBA - (1,2,3,0.4)</li>
+                    <li value="hex">HEX (#AA1923)</li>
+                    <li value="hexNoHash">HEX (AA1923)</li>
+                    <li value="rgb">RGB - (1,2,3)</li>
+                    <li value="rgba">RGBA - (1,2,3,0.4)</li>
                 </ul>
             </div>
             <div className="sound-btn" onClick={onClick}>Sound {`${isMuted ? 'Off 🔇' : 'On 🔊'}`}</div>
