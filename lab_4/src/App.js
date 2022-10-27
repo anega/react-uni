926c892b0674b896de6e5df35702bc1e20c0231f
anegin <anegamega@gmail.com>
2022-10-27 13:42:08 +0300

    Lab4: minimal router setup.

--- lab_4/src/App.js
+++ lab_4/src/App.js
@@ -1,9 +1,16 @@
+import {Route, Routes} from "react-router-dom";
+import Layout from "./pages/Layout/Layout";
+import NotFound from "./pages/NotFound/NotFound";
 import './App.css';
 
 function App() {
-  return (
-    <div>App</div>
-  );
+    return (
+        <Routes>
+            <Route path="/" element={<Layout/>}>
+                <Route path="*" element={<NotFound/>}/>
+            </Route>
+        </Routes>
+    );
 }
 
 export default App;
---
+++ lab_4/src/pages/NotFound/NotFound.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NotFound = () => (
+    <div className="flex justify-center items-center">
+        <h1>404 Not found.</h1>
+    </div>
+);
+
+export default NotFound;
