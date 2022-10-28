bf6de476bd11253d6a50912ca65cf4f2999f2d44
anegin <anegamega@gmail.com>
2022-10-28 22:43:10 +0300

    Lab4: style NotFound.

--- lab_4/src/pages/NotFound/NotFound.js
+++ lab_4/src/pages/NotFound/NotFound.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const NotFound = () => (
-    <div className="flex justify-center items-center">
+    <div className="flex justify-center items-center pt-32 pb-12">
         <h1>404 Not found.</h1>
     </div>
 );
