4820027d73dd7d4fa4926454bf503d0b5495edac
anegin <anegamega@gmail.com>
2022-11-04 21:44:49 +0200

    Lab4: add custom background tailwind class.

--- lab_4/tailwind.config.js
+++ lab_4/tailwind.config.js
@@ -8,8 +8,11 @@ module.exports = {
       screens: {
         'xl-custom': '1248px',
       },
+      backgroundImage: {
+        'post-img-text': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.4) 100%)',
+      },
       boxShadow: {
-        'bigbox': '0px 20px 24px -4px rgba(16, 24, 40, 0.08), 0px 8px 8px -4px rgba(16, 24, 40, 0.03)'
+        'bigbox': '0px 20px 24px -4px rgba(16, 24, 40, 0.08), 0px 8px 8px -4px rgba(16, 24, 40, 0.03)',
       },
     },
   },
