1691c3dc7fcb55ea0cc94d3972cc59a66529ed37
anegin <anegamega@gmail.com>
2022-11-06 20:19:53 +0200

    Lab4: something changed in App.js.

--- lab_4/src/App.js
+++ lab_4/src/App.js
@@ -2,8 +2,8 @@ import {Route, Routes} from "react-router-dom";
 import Layout from "./pages/Layout/Layout";
 import Home from "./pages/Home/Home";
 import NotFound from "./pages/NotFound/NotFound";
-import './App.css';
 import Categories from "./pages/Categories/Categories";
+import './App.css';
 
 function App() {
     return (
@@ -12,7 +12,7 @@ function App() {
                 <Route index element={<Home/>}/>
                 <Route path="categories" element={<Categories/>}/>
                 <Route path="categories/:categoryName" element={<Categories/>}/>
-                <Route path="post/:id" element={<>test post</>}/>
+                <Route path="post/:id" element={<p>test post</p>}/>
                 <Route path="*" element={<NotFound/>}/>
             </Route>
         </Routes>
