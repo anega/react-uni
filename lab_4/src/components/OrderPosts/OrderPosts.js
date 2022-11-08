d89914a5d064263c3736e6ac15b73d1a70401dec
anegin <anegamega@gmail.com>
2022-11-08 17:06:54 +0200

    Lab4: add order posts component.

---
+++ lab_4/src/components/OrderPosts/OrderPosts.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import {AiOutlineArrowDown, AiOutlineArrowUp, AiOutlineSortAscending, AiOutlineSortDescending} from 'react-icons/ai';
+import Select, {components} from 'react-select';
+
+const options = [
+    {
+        value: 'asc',
+        label: 'Ascending',
+        icon: <AiOutlineArrowDown className="inline-block mr-2 mb-0.5" color="#667085" size="18"/>
+    },
+    {
+        value: 'desc',
+        label: 'Descending',
+        icon: <AiOutlineArrowUp className="inline-block mr-2 mb-0.5" color="#667085" size="18"/>
+    },
+    {
+        value: 'abc',
+        label: 'Ascending by title',
+        icon: <AiOutlineSortAscending className="inline-block mr-2 mb-0.5" color="#667085" size="18"/>
+    },
+    {
+        value: 'cba',
+        label: 'Descending by title',
+        icon: <AiOutlineSortDescending className="inline-block mr-2 mb-0.5" color="#667085" size="18"/>
+    },
+];
+
+const SingleValue = ({children, ...props}) => (
+    <components.SingleValue {...props}>{props.data.icon}{children}</components.SingleValue>
+);
+
+const Option = ({children, ...props}) => (
+    <components.Option {...props}>{props.data.icon}{children}</components.Option>
+);
+
+const selectStyles = {
+    container: (provided, state) => ({
+        ...provided,
+        minWidth: '240px',
+    }),
+    control: (provided, state) => ({
+        ...provided,
+        border: '1px solid #d0d5dd',
+        borderRadius: '8px',
+        padding: '4px',
+        outline: 'none',
+    }),
+    indicatorSeparator: () => null,
+    dropdownIndicator: (provided, state) => ({
+        ...provided,
+        color: '#667085',
+    }),
+    singleValue: (provided, state) => ({
+        ...provided,
+        color: '#101828',
+    }),
+};
+
+const OrderPosts = () => {
+    return (
+        <Select options={options}
+                defaultValue={options[0]}
+                components={{SingleValue, Option}}
+                isSearchable={false}
+                styles={selectStyles}/>
+    );
+};
+
+export default OrderPosts;
