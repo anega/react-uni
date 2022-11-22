42fb08a6013d0de4801764ca175d72bbf2b9eaee
anegin <anegamega@gmail.com>
2022-11-22 22:13:42 +0200

    Lab5: add style group for logical parts of the forms.

---
+++ lab_5/src/components/FieldGroup/FieldGroup.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './FieldGroup.css';
+
+export const FieldGroup = (props) => (
+    <div className="field-group">{props.children}</div>
+);
---
+++ lab_5/src/components/FieldGroup/index.js
@@ -0,0 +1,1 @@
+export {FieldGroup as default} from './FieldGroup';
