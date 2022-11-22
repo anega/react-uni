5b60b825bcb8677eeebae8b442f91bd7337d4044
anegin <anegamega@gmail.com>
2022-11-22 19:26:54 +0200

    Lab5: setup simple project structure.

--- lab_5/src/App.js
+++ lab_5/src/App.js
@@ -3,7 +3,23 @@ import React from 'react';
 const App = () => {
     return (
         <div>
-          Setup
+            <header>
+                <h1>logo</h1>
+                <div>form steps</div>
+            </header>
+            <div>
+                <h1>Registration</h1>
+                <p>Fill in the registration data. It will take a couple of minutes. All you need is a phone number and e-mail</p>
+                <form action="">
+                    <div>step 1</div>
+                    <div>step 2</div>
+                    <div>step 3</div>
+                    <div>step 4</div>
+                    <div>step 5</div>
+                    <div>step 6</div>
+                    <button>button</button>
+                </form>
+            </div>
         </div>
     );
 };
