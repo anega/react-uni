2bb0e25b97a647232c811af4df62d9fa4b7399b7
anegin <anegamega@gmail.com>
2022-11-22 22:14:54 +0200

    Lab5: add common styles; use created components.

--- lab_5/src/pages/Layout/Layout.js
+++ lab_5/src/pages/Layout/Layout.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import {MdLock, MdOutlineClose} from 'react-icons/md';
 import Header from 'components/Header';
 import StepIndicator from 'components/StepIndicator';
+import StepInfo from 'components/StepInfo';
+import InfoMessage from 'components/InfoMessage';
 import './Layout.css';
+import FieldGroup from '../../components/FieldGroup';
 
 export const Layout = () => {
     return (
@@ -9,9 +13,16 @@ export const Layout = () => {
             <Header/>
             <div className="content">
                 <StepIndicator/>
-                <h1>Registration</h1>
-                <p>Fill in the registration data. It will take a couple of minutes. All you need is a phone number and
-                    e-mail</p>
+                <StepInfo
+                    title="Registration"
+                    description="Fill in the registration data. It will take a couple of minutes. All you need is a phone number and e-mail"/>
+                <InfoMessage
+                    prefixIcon={<MdLock/>}
+                    message="We take privacy issues seriously. You can be sure that your personal data is securely protected."
+                    suffixIcon={<MdOutlineClose/>}/>
+                <FieldGroup>
+                    <p className="input-label">Enter your phone number</p>
+                </FieldGroup>
                 <form action="">
                     <div>step 1</div>
                     <div>step 2</div>
