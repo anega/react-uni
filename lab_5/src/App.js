ebfe41866a1f1b20673e7de12a11803a338536b5
anegin <anegamega@gmail.com>
2022-11-22 21:00:44 +0200

    Lab5: refactor - move code from App to Layout page.

--- lab_5/src/App.js
+++ lab_5/src/App.js
@@ -1,28 +1,10 @@
 import React from 'react';
-import Header from 'components/Header';
+import Layout from 'pages/Layout';
 import './App.css';
-import StepIndicator from './components/StepIndicator/StepIndicator';
 
 const App = () => {
     return (
-        <>
-            <Header/>
-            <div>
-                <StepIndicator/>
-                <h1>Registration</h1>
-                <p>Fill in the registration data. It will take a couple of minutes. All you need is a phone number and
-                    e-mail</p>
-                <form action="">
-                    <div>step 1</div>
-                    <div>step 2</div>
-                    <div>step 3</div>
-                    <div>step 4</div>
-                    <div>step 5</div>
-                    <div>step 6</div>
-                    <button>button</button>
-                </form>
-            </div>
-        </>
+        <Layout/>
     );
 };
 
---
+++ lab_5/src/pages/Layout/Layout.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Header from 'components/Header';
+import StepIndicator from 'components/StepIndicator';
+
+const Layout = () => {
+    return (
+        <>
+            <Header/>
+            <div className="content">
+                <StepIndicator/>
+                <h1>Registration</h1>
+                <p>Fill in the registration data. It will take a couple of minutes. All you need is a phone number and
+                    e-mail</p>
+                <form action="">
+                    <div>step 1</div>
+                    <div>step 2</div>
+                    <div>step 3</div>
+                    <div>step 4</div>
+                    <div>step 5</div>
+                    <div>step 6</div>
+                    <button>button</button>
+                </form>
+            </div>
+        </>
+    );
+};
+
+export default Layout;
