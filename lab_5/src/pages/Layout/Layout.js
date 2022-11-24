dde139ca539793f4ae21d4b3d758b983124653dc
anegin <anegamega@gmail.com>
2022-11-24 18:24:08 +0200

    Lab5: set first step submission functionality.

--- lab_5/src/pages/Layout/Layout.js
+++ lab_5/src/pages/Layout/Layout.js
@@ -1,28 +1,45 @@
 import React, {useState} from 'react';
+import {FormProvider, useForm} from 'react-hook-form';
 import Header from 'components/Header';
-import './Layout.css';
 import PhoneStep from '../../components/PhoneStep';
+import './Layout.css';
 
 export const Layout = () => {
+    const methods = useForm();
     const [step, setStep] = useState(0);
+    const [userData, setUserData] = useState({});
 
     const handleNextStep = () => {
-        setStep((prevState) => prevState++);
+        const formData = methods.getValues();
+        setUserData((prevState) => ({
+            ...prevState,
+            formData,
+        }));
+        setStep((prevState) => prevState + 1);
+    };
+
+    const onSubmit = (formData) => {
+        setUserData((prevState) => ({
+            ...prevState,
+            formData,
+        }));
     };
 
     return (
         <>
             <Header/>
             <div className="content">
-                <form action="">
-                    {step === 0 && <PhoneStep handleNextStep={handleNextStep}/>}
-                    <div>step 2</div>
-                    <div>step 3</div>
-                    <div>step 4</div>
-                    <div>step 5</div>
-                    <div>step 6</div>
-                    <button>button</button>
-                </form>
+                <FormProvider {...methods}>
+                    <form onSubmit={methods.handleSubmit(onSubmit)}>
+                        {step === 0 && <PhoneStep handleNextStep={handleNextStep}/>}
+                        {step === 1 && <div>step 2</div>}
+                        <div>step 3</div>
+                        <div>step 4</div>
+                        <div>step 5</div>
+                        <div>step 6</div>
+                        <button type="submit">button</button>
+                    </form>
+                </FormProvider>
             </div>
         </>
     );
