a9c21bbdd1be399869da4f24f3ebb722b6029336
anegin <anegamega@gmail.com>
2022-11-26 22:54:34 +0200

    Lab5: add service to call countries and cities data lists.

---
+++ lab_5/src/services/LocationService.js
@@ -0,0 +1,23 @@
+export const getCountries = async () => {
+    try {
+        const response = await fetch('https://countriesnow.space/api/v0.1/countries/info?returns=name');
+        return response.json();
+    } catch (e) {
+        return e;
+    }
+};
+
+export const getCities = async (country) => {
+    try {
+        console.log('getCities service')
+        const response = await fetch('https://countriesnow.space/api/v0.1/countries/cities', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({country: country.toLowerCase()})
+        });
+
+        return await response.json();
+    } catch (e) {
+        return e;
+    }
+};
