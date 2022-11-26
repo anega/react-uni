4564d737094cf1b4117cdd5954df2490f2bcaf60
anegin <anegamega@gmail.com>
2022-11-26 22:51:00 +0200

    Lab5: use 5th step in Layout.

--- lab_5/src/pages/Layout/Layout.js
+++ lab_5/src/pages/Layout/Layout.js
@@ -1,9 +1,9 @@
 import React, {useState} from 'react';
 import {FormProvider, useForm} from 'react-hook-form';
 import Header from 'components/Header';
-import PhoneStep from '../../components/PhoneStep';
 import ConfirmPhoneStep from '../../components/ConfirmPhoneStep';
 import EmailPassStep from '../../components/EmailPassStep';
+import PersonalDataStep from '../../components/PersonalDataStep';
 import './Layout.css';
 
 export const Layout = () => {
@@ -27,8 +27,7 @@ export const Layout = () => {
                         {step === 1 && <PhoneStep handleNextStep={handleNextStep}/>}
                         {step === 2 && <ConfirmPhoneStep handleNextStep={handleNextStep}/>}
                         {step === 3 && <EmailPassStep handleNextStep={handleNextStep}/>}
-                        <div>step 3</div>
-                        <div>step 4</div>
+                        {step === 4 && <PersonalDataStep handleNextStep={handleNextStep}/>}
                         <div>step 5</div>
                         <div>step 6</div>
                         <button type="submit">button</button>
