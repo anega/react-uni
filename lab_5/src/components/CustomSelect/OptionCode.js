6d86cafdf69865ecc1f6b8f6e90437cfd97fb236
anegin <anegamega@gmail.com>
2022-11-27 23:31:45 +0200

    Lab5: customize structure, add styles to the CustomSelect custom components.

--- lab_5/src/components/CustomSelect/OptionCode.js
+++ lab_5/src/components/CustomSelect/OptionCode.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import {components} from 'react-select';
+import './styles.css';
 
 const OptionCode = ({children, ...props}) => (
     <components.Option {...props}
-                       innerProps={Object.assign({}, props.innerProps, {'data-country': props.data.countryName})}>{children}</components.Option>
+                       innerProps={Object.assign({}, props.innerProps, {'data-country': props.data.countryName})}>
+        {props.data.icon && <span className="select-icon">{props.data.icon}</span>}
+        <span className="select-option-text">{children}</span>
+    </components.Option>
 );
 
 export default OptionCode;
--- lab_5/src/components/CustomSelect/SingleValueCode.js
+++ lab_5/src/components/CustomSelect/SingleValueCode.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import {components} from 'react-select';
+import './styles.css';
 
 const SingleValueCode = ({children, ...props}) => (
     <components.SingleValue {...props}
-                            innerProps={Object.assign({}, props.innerProps, {'data-country': props.data.countryName})}>{children}</components.SingleValue>
+                            innerProps={Object.assign({}, props.innerProps, {'data-country': props.data.countryName})}>
+        {props.data.icon && <span className="select-icon">{props.data.icon}</span>}
+        <span className="select-option-text">{children}</span>
+    </components.SingleValue>
 );
 
 export default SingleValueCode;
