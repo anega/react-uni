dbe7c08cdc30b303dbb8360b74680766dcd627db
anegin <anegamega@gmail.com>
2022-11-27 19:19:35 +0200

    Lab5: add icon to the Button component.

--- lab_5/src/components/Button/Button.js
+++ lab_5/src/components/Button/Button.js
@@ -3,6 +3,9 @@ import './Button.css';
 
 export const Button = ({className, value, onClick, icon, buttonType}) => {
     return (
-        <button type={buttonType} className={className} onClick={onClick}>{value}{icon}</button>
+        <button type={buttonType} className={className} onClick={onClick}>
+            {value}
+            {icon && <span className="btn-icon">{icon}</span>}
+        </button>
     );
 };
