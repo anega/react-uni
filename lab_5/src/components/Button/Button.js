fbcd7e9f7d91dab26eb2741c590952d5c1b887c1
anegin <anegamega@gmail.com>
2022-11-28 15:24:58 +0200

    Lab5: diferentiate prefix and suffix button icons in Button component.

--- lab_5/src/components/Button/Button.js
+++ lab_5/src/components/Button/Button.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './Button.css';
 
-export const Button = ({className, value, onClick, icon, buttonType}) => {
+export const Button = ({className, value, onClick, prefixIcon, icon, buttonType}) => {
     return (
         <button type={buttonType} className={className} onClick={onClick}>
+            {prefixIcon && <span className="btn-prefix-icon">{prefixIcon}</span>}
             {value}
-            {icon && <span className="btn-icon">{icon}</span>}
+            {icon && <span className="btn-suffix-icon">{icon}</span>}
         </button>
     );
 };
