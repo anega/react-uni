da3fbd28f635dd2455146ae835c1bfdd19e407ac
anegin <anegamega@gmail.com>
2022-11-28 22:03:15 +0200

    Lab5: add validation for the PersonalDataStep(4th).

--- lab_5/src/components/CountryCityFields/CountryCityFields.js
+++ lab_5/src/components/CountryCityFields/CountryCityFields.js
@@ -3,10 +3,11 @@ import {debounce} from 'lodash';
 import {Controller, useFormContext} from 'react-hook-form';
 import {getCities, getCountries} from '../../services/LocationService';
 import {CustomAsyncSelect} from '../CustomSelect/CustomAsyncSelect';
+import FormFieldError from '../FormFieldError';
 import './CountryCityFields.css';
 
 export const CountryCityFields = ({label, countryFieldName, cityFieldName}) => {
-    const {watch, control} = useFormContext();
+    const {watch, control, formState: {errors}} = useFormContext();
     const country = watch(countryFieldName);
 
     const fetchCountries = async (inputValue) => {
@@ -46,23 +47,35 @@ export const CountryCityFields = ({label, countryFieldName, cityFieldName}) => {
         <div className="field-wrap">
             <p className="input-label">{label}</p>
             <div className="place-birth-wrap">
-                <Controller
-                    control={control}
-                    name={countryFieldName}
-                    render={({field}) => (
-                        <CustomAsyncSelect placeholder="Country"
-                                           onChange={field.onChange}
-                                           loadOptions={loadCountryOptions}/>
-                    )}/>
-                <Controller
-                    control={control}
-                    name={cityFieldName}
-                    render={({field}) => (
-                        <CustomAsyncSelect componentKey={JSON.stringify(country)}
-                                           placeholder="City"
-                                           onChange={field.onChange}
-                                           loadOptions={loadCityOptions}/>
-                    )}/>
+                <div>
+                    <Controller
+                        control={control}
+                        name={countryFieldName}
+                        rules={{
+                            required: 'Country field is required.'
+                        }}
+                        render={({field}) => (
+                            <CustomAsyncSelect placeholder="Country"
+                                               onChange={field.onChange}
+                                               loadOptions={loadCountryOptions}/>
+                        )}/>
+                    {errors[countryFieldName] && <FormFieldError errorMessage={errors[countryFieldName].message}/>}
+                </div>
+                <div>
+                    <Controller
+                        control={control}
+                        name={cityFieldName}
+                        rules={{
+                            required: 'City field is required.'
+                        }}
+                        render={({field}) => {
+                            return <CustomAsyncSelect componentKey={JSON.stringify(country)}
+                                                      placeholder="City"
+                                                      onChange={field.onChange}
+                                                      loadOptions={loadCityOptions}/>
+                        }}/>
+                    {errors[cityFieldName] && <FormFieldError errorMessage={errors[cityFieldName].message}/>}
+                </div>
             </div>
         </div>
     );
--- lab_5/src/components/PersonalDataStep/PersonalDataStep.js
+++ lab_5/src/components/PersonalDataStep/PersonalDataStep.js
@@ -11,12 +11,13 @@ import Button from '../Button';
 import {AiOutlineArrowRight, AiOutlineCheck} from 'react-icons/ai';
 import './PersonalDataStep.css';
 import CountryCityFields from '../CountryCityFields';
+import FormFieldError from '../FormFieldError';
 
 const formIndicatorStep = 1;
 const nextFormStep = 5;
 
 export const PersonalDataStep = ({handleNextStep}) => {
-    const {register, control} = useFormContext();
+    const {register, control, formState: {errors, isDirty, isValid}} = useFormContext();
 
     return (
         <>
@@ -25,28 +26,37 @@ export const PersonalDataStep = ({handleNextStep}) => {
                 title="Profile info"
                 description="Fill in the data for profile. It will take a couple of minutes. You only need a passport"/>
             <label htmlFor="terms" className="inline-checkbox">
-                <input {...register('termsofuse')} id="terms" type="checkbox"/>
+                <input {...register('termsofuse', {
+                    required: 'This field is required.'
+                })} id="terms" type="checkbox"/>
                 <span>I agree with <a href="https://www.google.com/" target="_blank"
                                       rel="noreferrer">Terms of use</a></span>
             </label>
+            {errors.termsofuse && <FormFieldError errorMessage={errors.termsofuse.message}/>}
             <FieldGroup>
                 <FieldGroupInfo title="Personal data" label="Specify exactly as in your passport"/>
                 <div className="field-wrap">
                     <p className="input-label">First name</p>
-                    <input {...register('firstName')} type="text" className="text-input"/>
+                    <input {...register('firstName', {
+                        required: 'First name is required.'
+                    })} type="text" className="text-input"/>
+                    {errors.firstName && <FormFieldError errorMessage={errors.firstName.message}/>}
                 </div>
                 <div className="field-wrap">
                     <p className="input-label">Second name</p>
-                    <input {...register('secondName')} type="text" className="text-input"/>
+                    <input {...register('secondName', {
+                        required: 'Second name is required.'
+                    })} type="text" className="text-input"/>
+                    {errors.secondName && <FormFieldError errorMessage={errors.secondName.message}/>}
                 </div>
                 <CountryCityFields label="Place of birth"
-                                   countryFieldName="birth.country"
-                                   cityFieldName="birth.city"/>
+                                   countryFieldName="birthCountry"
+                                   cityFieldName="birthCity"/>
                 <div className="field-wrap">
                     <p className="input-label">Date of birth</p>
                     <Controller
                         control={control}
-                        name="birth.date"
+                        name="birthDate"
                         render={({field}) => (
                             <DatePicker wrapperClassName="date-picker"
                                         selected={field.value}
@@ -61,7 +71,11 @@ export const PersonalDataStep = ({handleNextStep}) => {
             <Button className="btn outline-btn"
                     value="Go Next"
                     icon={<AiOutlineArrowRight size="14" color="#242731"/>}
-                    onClick={() => handleNextStep(nextFormStep)}/>
+                    onClick={() => {
+                        // console.log(isValid)
+                        // console.log(isDirty)
+                        if (isValid && isDirty) handleNextStep(nextFormStep);
+                    }}/>
         </>
     );
 };
